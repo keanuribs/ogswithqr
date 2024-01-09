5abd48e6cd4693954b9e9ec7258ca12d94ab94ba
DDMXD <fortezajollyvher3@gmal.com>
2024-01-09 19:52:47 +0800

    start ako bukas ng umaga

---
+++ dcs/grading/lecture/lecture-only.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  .table-container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+  }
+
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    width: 48%;
+  }
+
+  td {
+    width: 4%;
+    text-align: center;
+  }
+
+  .table-container > div:first-child {
+    margin-right: 2%;
+  }
+</style>
+</head>
+<body>
+
+<div class="table-container">
+  <div>
+
+    <table>
+      <tr>
+        <th colspan="6">Attendance</th>
+        <th colspan="36">Quiz/Long Exam</th>
+        <th colspan="36">OUTPUT / PORTFOLIO</th>
+        <th colspan="6">Midterm</th>
+      </tr>
+      <tr>
+        <!-- Attendance section -->
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td>
+        <!-- Quiz/Long Exam section -->
+        <td colspan="3">Quiz 1</td>
+        <td colspan="3">Quiz 2</td>
+        <td colspan="3">Quiz 3</td>
+        <td colspan="3">Quiz 4</td>
+        <td colspan="3">Quiz 5</td>
+        <td colspan="3">Quiz 6</td>
+        <td colspan="3">Quiz 7</td>
+        <td colspan="3">Quiz 8</td>
+        <td colspan="3">Quiz 9</td>
+        <td colspan="3">Quiz 10</td>
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td>
+        <!-- OUTPUT / PORTFOLIO section -->
+        <td colspan="3">1</td>
+        <td colspan="3">2</td>
+        <td colspan="3">3</td>
+        <td colspan="3">4</td>
+        <td colspan="3">5</td>
+        <td colspan="3">6</td>
+        <td colspan="3">7</td>
+        <td colspan="3">8</td>
+        <td colspan="3">9</td>
+        <td colspan="3">10</td>
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td>
+        <!-- midterm -->
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td> 
+      </tr>
+    </table>
+
+  </div>
+
+  <div>
+
+    <table>
+      <tr>
+        <th colspan="2">Attendance</th>
+        <th colspan="36">Quiz/Long Exam</th>
+        <th colspan="36">OUTPUT / PORTFOLIO</th>
+        <th colspan="6">Midterm</th>
+        <th colspan="6">Finals</th>
+      </tr>
+      <tr>
+        <!-- Attendance section -->
+        <td>Total</td>
+        <td>Weight</td>
+        <td colspan="3">Quiz 1</td>
+        <td colspan="3">Quiz 2</td>
+        <td colspan="3">Quiz 3</td>
+        <td colspan="3">Quiz 4</td>
+        <td colspan="3">Quiz 5</td>
+        <td colspan="3">Quiz 6</td>
+        <td colspan="3">Quiz 7</td>
+        <td colspan="3">Quiz 8</td>
+        <td colspan="3">Quiz 9</td>
+        <td colspan="3">Quiz 10</td>
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td>
+        <!-- OUTPUT / PORTFOLIO section -->
+        <td colspan="3">1</td>
+        <td colspan="3">2</td>
+        <td colspan="3">3</td>
+        <td colspan="3">4</td>
+        <td colspan="3">5</td>
+        <td colspan="3">6</td>
+        <td colspan="3">7</td>
+        <td colspan="3">8</td>
+        <td colspan="3">9</td>
+        <td colspan="3">10</td>
+        <td colspan="3">Total</td>
+        <td colspan="3">Weight</td>
+        <!-- midterm -->
+        <td colspan="3">Score</td>
+        <td colspan="3">Weight</td>  
+        <!-- midterm -->
+        <td colspan="3">Score</td>
+        <td colspan="3">Weight</td> 
+      </tr>
+    </table>
+
+  </div>
+</div>
+
+</body>
+</html>
